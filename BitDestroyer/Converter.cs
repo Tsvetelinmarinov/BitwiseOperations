6f3d29320ab9b1f13573e9ff5188a4008e22f472
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-07 22:01:54 +0300

    Solution 2

---
+++ BitDestroyer/Converter.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * BitDestroyer
+ */
+
+namespace BitDestroyer
+{
+    class Converter
+    {
+        public static string ConvertToBinary(int number) 
+        {
+            List<byte> bits = new();
+            while (number > 0)
+            {
+                bits.Add((byte)(number % 2));
+                number /= 2;
+            }
+
+            bits.Reverse();
+            return string.Join("", bits);
+        }
+        public static double ConvertToDecimal(string binary)
+        {
+            List<int> bits = new();
+            List<double> decimalBits = new();
+
+            foreach (char bit in binary)
+            {
+                bits.Add(int.Parse(bit.ToString()));
+            }
+
+            double power = bits.Count - 1;
+            foreach (int bit in bits)
+            {
+                decimalBits.Add(bit * Math.Pow(2, power--));
+            }
+
+            return decimalBits.Sum();
+        }
+        public static string ModifyBit(string binary, int bitPosition)
+        {
+            List<int> bits = new();
+            foreach (char bit in binary)
+            {
+                bits.Add(int.Parse(bit.ToString()));
+            }
+
+            bits[bits.Count - 1 - bitPosition] = 0;
+            return string.Join("", bits);
+        }
+    }
+}
---
+++ BitDestroyer/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * BitDestroyer
+ */
+namespace BitDestroyer;
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine()!);
+        int position = int.Parse(Console.ReadLine()!);
+
+        string binary = Converter.ConvertToBinary(number);
+        binary = Converter.ModifyBit(binary, position);
+        double result = Converter.ConvertToDecimal(binary);
+
+        Console.WriteLine(result);
+    }
+};
+
+
