4005ae84f652a2a457eac4ad53802e9e5b5deed5
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-08 18:10:09 +0300

    Solution 5

---
+++ OddTimes/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * OddTimes
+ */
+
+using System.Net.Http.Headers;
+
+namespace OddOccurence;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()!
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        Console.WriteLine(GetOddOccurence(numbers));
+    }
+    
+    static int GetOddOccurence(List<int> collection)
+    {
+        for (int i = 0; i < collection.Count; i++)
+        {
+            int counter = default;
+            for (int j = 0; j < collection.Count; j++)
+            {
+                if (collection[i] == collection[j])
+                {
+                    counter++;
+                }
+            }
+
+            if (counter % 2 != 0)
+            {
+                return collection[i];
+            }
+
+            counter = 0;
+        }
+
+        return 0;
+    }
+};
